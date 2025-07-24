f7efc3bf8d8e9301b1d890e3627eb411355bb6e5
jeena-reji <167992298+jeena-reji@users.noreply.github.com>
2025-07-24 15:16:48 +0530

    Update abot_runner.py

--- abot_runner.py
+++ abot_runner.py
@@ -10,7 +10,7 @@ SUMMARY_URL = f"{ABOT_URL}/abot/api/v5/artifacts/execFeatureSummary"
 
 USERNAME = "ajeesh@cazelabs.com"
 PASSWORD = "ajeesh1234"
-FEATURE_TAG = "IOS_MCN_5GC_Pre_Release0.1.2"
+FEATURE_TAG = "000-local-commands.feature"
 CONFIG_FILE = "/etc/rebaca-test-suite/config/admin/ABotConfig.properties"
 
 headers = {"Content-Type": "application/json"}
