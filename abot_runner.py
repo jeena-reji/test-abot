a4afa40005ff2df9bb755fb2e962579ed5e19c0a
jeena-reji <167992298+jeena-reji@users.noreply.github.com>
2025-08-18 09:35:33 +0530

    Update abot_runner.py

--- abot_runner.py
+++ abot_runner.py
@@ -30,7 +30,7 @@ def update_config():
     print("Updating config...")
     payload = {
         "update": {
-            "ABOT.TESTBED": "testbed-5g-IOSMCN=emulated-smf+sut-upf"
+            "ABOT.TESTBED": "testbed-4g5g"
         }
     }
     res = requests.post(CONFIG_URL, headers=headers, json=payload, params={"filename": CONFIG_FILE})
